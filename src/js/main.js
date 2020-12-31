a6796568aae2ee3cb8e00f7f6295b9d299733baf
Francuski Miroslav <francuski.miroslav@gmail.com>
2020-12-31 21:05:02 +0100

    added dynamic year in background

--- src/js/main.js
+++ src/js/main.js
@@ -22,10 +22,13 @@ document.addEventListener('DOMContentLoaded', function () {
 	const minutes = document.getElementById('minutes');
 	const seconds = document.getElementById('seconds');
 	const countdown = document.getElementById('countdown');
+	const year = document.getElementById('year');
 
 	const currentYear = new Date().getFullYear();
 
 	const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+	year.innerText = currentYear + 1;
 
 	function updateCountdown() {
 		const currentTime = new Date();
@@ -38,9 +41,9 @@ document.addEventListener('DOMContentLoaded', function () {
 		const s = Math.floor(diff / 1000) % 60;
 
 		days.innerHTML = d;
-		hours.innerHTML = h;
-		minutes.innerHTML = m;
-		seconds.innerHTML = s;
+		hours.innerHTML = h < 10 ? '0' + h : h;
+		minutes.innerHTML = m < 10 ? '0' + m : m;
+		seconds.innerHTML = s < 10 ? '0' + s : s;
 	}
 
 	setInterval(updateCountdown, 1000);
